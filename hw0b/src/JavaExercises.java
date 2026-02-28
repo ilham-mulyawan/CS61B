69e3c480ad77291810b507fb2b494dd531039b15
Ilham Rizki Mulyawan <ilham.r.mulyawan@gmail.com>
2026-02-28 13:36:08 +0700

    hw0b: Completed JavaExercises.java and ListExercises.java

--- hw0b/src/JavaExercises.java
+++ hw0b/src/JavaExercises.java
@@ -1,3 +1,6 @@
+import org.apache.commons.lang3.ObjectUtils;
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -5,8 +8,8 @@ public class JavaExercises {
 
     /** Returns an array [1, 2, 3, 4, 5, 6] */
     public static int[] makeDice() {
-        // TODO: Fill in this function.
-        return null;
+        int[] diceArray = {1, 2, 3, 4, 5, 6};
+        return diceArray;
     }
 
     /** Returns the order depending on the customer.
@@ -14,15 +17,32 @@ public class JavaExercises {
      *  If the customer is Erik, return ["sushi", "pasta", "avocado", "coffee"].
      *  In any other case, return an empty String[] of size 3. */
     public static String[] takeOrder(String customer) {
-        // TODO: Fill in this function.
-        return null;
+        
+        if (customer.equals("Ergun")) {
+            return new String[]{"beyti", "pizza", "hamburger", "tea"};
+        } else if (customer.equals("Erik")) {
+            return new String[]{"sushi", "pasta", "avocado", "coffee"};
+        } else {
+            return new String[3];
+        }
     }
 
     /** Returns the positive difference between the maximum element and minimum element of the given array.
      *  Assumes array is nonempty. */
     public static int findMinMax(int[] array) {
-        // TODO: Fill in this function.
-        return 0;
+        int lowest = array[0];
+        int highest = array[0];
+
+        for (int i = 0; i < array.length; i += 1) {
+            if (lowest > array[i]) {
+                lowest = array[i];
+            }
+            if (highest < array[i]) {
+                highest = array[i];
+            }
+        }
+
+        return highest - lowest;
     }
 
     /**
@@ -38,8 +58,16 @@ public class JavaExercises {
     }
 
     private static List<Integer> hailstoneHelper(int x, List<Integer> list) {
-        // TODO: Fill in this function.
-        return null;
+        list.add(x);
+
+        if (x == 1) {
+            return list;
+        }
+        if (x % 2 == 0) {
+            return hailstoneHelper(x / 2, list);
+        } else {
+            return hailstoneHelper((x * 3) + 1, list);
+        }
     }
 
 }
--- hw0b/src/ListExercises.java
+++ hw0b/src/ListExercises.java
@@ -1,29 +1,54 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListExercises {
 
     /** Returns the total sum in a list of integers */
     public static int sum(List<Integer> L) {
-        // TODO: Fill in this function.
-        return 0;
+        if (L.isEmpty()) {
+            return 0;
+        } else {
+            int sum = 0;
+            for (int i : L) {
+                sum += i;
+            }
+            return sum;
+        }
     }
 
     /** Returns a list containing the even numbers of the given list */
     public static List<Integer> evens(List<Integer> L) {
-        // TODO: Fill in this function.
-        return null;
+        List<Integer> lst = new ArrayList<>();
+        for (int i: L) {
+            if (i % 2 == 0) {
+                lst.add(i);
+            }
+        }
+        return lst;
     }
 
     /** Returns a list containing the common item of the two given lists */
     public static List<Integer> common(List<Integer> L1, List<Integer> L2) {
-        // TODO: Fill in this function.
-        return null;
+        List<Integer> lst = new ArrayList<>();
+        for (int i: L1) {
+            if (L2.contains(i)) {
+                lst.add(i);
+            }
+        }
+        return lst;
     }
 
 
     /** Returns the number of occurrences of the given character in a list of strings. */
     public static int countOccurrencesOfC(List<String> words, char c) {
-        // TODO: Fill in this function.
-        return 0;
+        int count = 0;
+        for (String word: words) {
+            for (int i = 0; i < word.length(); i += 1) {
+                if (c == word.charAt(i)) {
+                    count += 1;
+                }
+            }
+        }
+        return count;
     }
 }
