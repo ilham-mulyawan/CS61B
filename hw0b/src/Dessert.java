b8f31283d73ad83457979635f710ab27cceca7ab
Ilham Rizki Mulyawan <ilham.r.mulyawan@gmail.com>
2026-02-28 15:55:35 +0700

    hw0b: Completed hw0b

---
+++ hw0b/src/Dessert.java
@@ -0,0 +1,19 @@
+public class Dessert {
+    public static int numDesserts = 0;
+    public int flavor;
+    public int price;
+
+    public Dessert(int flavor, int price) {
+        this.flavor = flavor;
+        this.price = price;
+        numDesserts += 1;
+    }
+
+    public void printDessert() {
+        System.out.println(flavor + " " + price + " " + numDesserts);
+    }
+
+    public static void  main(String[] args) {
+        System.out.println("I love dessert!");
+    }
+}
